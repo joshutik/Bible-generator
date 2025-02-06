d89a2c75debdb344c2a5101018a5df850d2dd2d4
joshutik <utik2.4real@gmail.com>
2025-02-06 22:28:57 +0100

    made a modification on the active links

--- src/Components/Regististration/Registration.jsx
+++ src/Components/Regististration/Registration.jsx
@@ -12,9 +12,9 @@ function Registration() {
     <div className="container-fluid reg">
       <div className="container pt-5">
       <div className="log-links d-flex justify-content-between w-25">
-        <button  className={`text-light text-decoration-none border-0 bg-transparent fs-3 fst-italic fw-bold ${isLogin ? "active-link" : ""}`}
+        <button  className={`text-light text-decoration-none login border-0 bg-transparent fs-3 fst-italic fw-bold ${isLogin ? "active-link" : ""}`}
         onClick={() => setIsLogin(true)}>Login</button>
-        <button className={`text-light text-decoration-none border-0 bg-transparent fs-3 fst-italic fw-bold ${isLogin ? "active-link" : ""}`}
+        <button className={`text-light text-decoration-none regis border-0 bg-transparent fs-3 fst-italic fw-bold ${isLogin ? "active-link" : ""}`}
         onClick={() => setIsLogin(false)}>Register</button>
       </div>
         <div className="row align-items-center mt-5">
